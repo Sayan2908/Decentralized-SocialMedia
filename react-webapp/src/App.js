1a5870d3cefe93aa203c9093cd585a99c1538e2b
Sayan Dey Sarkar <sayandeysarkar2003@gmail.com>
2023-10-18 02:56:01 +0530

    first commit

---
+++ react-webapp/src/App.js
@@ -0,0 +1,36 @@
+
+function App() {
+  return (
+    <div className="app">
+      <div className="navbar">
+        <div className="navbar__logo">
+          <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="Instagram logo" />
+        </div>
+        <div className="navbar__search">
+          <input type="text" placeholder="Search" />
+        </div>
+        <div className="navbar__menu">
+          <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="Instagram logo" />
+        </div>
+      </div>
+      <div className="hero">
+        <div className="hero__left">
+          <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="Instagram logo" />
+        </div>
+        <div className="hero__right">
+          <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="Instagram logo" />
+        </div>
+      </div>
+      <div className="contact">
+        <div className="contact__left">
+          <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="Instagram logo" />
+        </div>
+        <div className="contact__right">
+          <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="Instagram logo" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
